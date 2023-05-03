9c79c81fe39146f91c3ca9a819910c9b78ee3bd2
Maxim Alter <maxim_united@yahoo.com>
2023-05-03 15:39:11 +0300

    change import based on vscode suggesion

--- noip-renew.py
+++ noip-renew.py
@@ -17,7 +17,7 @@ from selenium import webdriver
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support.wait  import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from datetime import date
 from datetime import timedelta
